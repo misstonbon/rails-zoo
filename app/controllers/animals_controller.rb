448a90423b8a20eeb0c998df24d9234e85a4bf9e
Tanja Stroble <plusdeltah@gmail.com>
2017-10-01 23:08:08 -0700

    basic controller methods completed and corresponsing views

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,21 +1,52 @@
 class AnimalsController < ApplicationController
+
+before_action :find_animal, only: [:show, :edit, :update, :destroy]
+
   def index
+    @animals = Animal.all.order("created_at DESC")
+
+  end
+
+  def show
   end
 
   def new
+    @animal = Animal.new
   end
 
   def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @animal.update(animal_params)
+      redirect_to animal_path(@animal)
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @animal.destroy
+    redirect_to root_path  
   end
 
+private
+
+def animal_params
+  params.require(:animal).permit(:name, :species, :age)
+end
+
+def find_animal
+  @animal = Animal.find(params[:id])
+end
 
 end
